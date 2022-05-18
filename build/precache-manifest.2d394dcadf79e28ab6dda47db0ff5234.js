281eaa71acebb132bb71c2109acbb845ce2277d9
rifaq-ahmer <rifaq.ahmer@gmail.com>
2022-05-19 00:52:25 +0530

    Deployed the app on firebase

---
+++ build/precache-manifest.2d394dcadf79e28ab6dda47db0ff5234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4edc753b35e572f2bf2c",
+    "url": "/static/css/main.14d8d14b.chunk.css"
+  },
+  {
+    "revision": "4edc753b35e572f2bf2c",
+    "url": "/static/js/main.4edc753b.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f293d5c1122292b1c94a16230df6c3d",
+    "url": "/index.html"
+  }
+];
